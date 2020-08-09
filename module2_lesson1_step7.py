06c426293894bbb218c8d9f5bb0e225b71430191
AlexVenger <alexvenger456@gmail.com>
2020-08-09 22:51:36 +0400

    module 2 lesson 1 step 7 was added

---
+++ module2_lesson1_step7.py
@@ -0,0 +1,27 @@
+import math
+from selenium import webdriver
+import time
+
+
+def calc(x):
+    return str(math.log(abs(12*math.sin(int(x)))))
+
+
+link = 'http://suninjuly.github.io/get_attribute.html'
+browser = webdriver.Chrome()
+try:
+    browser.get(link)
+    x_element = browser.find_element_by_id('treasure')
+    x = x_element.get_attribute('valuex')
+    y = calc(x)
+    y_element = browser.find_element_by_id("answer")
+    y_element.send_keys(str(y))
+    check = browser.find_element_by_id('robotCheckbox')
+    check.click()
+    radio = browser.find_element_by_id('robotsRule')
+    radio.click()
+    button = browser.find_element_by_css_selector('button.btn')
+    button.click()
+finally:
+    time.sleep(10)
+    browser.quit()
